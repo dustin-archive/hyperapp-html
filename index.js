f893f186cc5766fe631028dc921ef4205e4cc25e
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2019-05-06 14:13:58 -0500

    0.4.0 (#4)
    * add cache & es5 syntax
    
    * rename tag to name

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 
-const vnode = (tag, data, children) => ({
-  nodeName: tag,
-  attributes: data,
-  children: Array.isArray(children) ? children : [children],
-  key: data.key
-})
+var cache = new Map()
 
-const h = (tag, data, children) =>
-  typeof data === 'object' && !Array.isArray(data)
-    ? vnode(tag, data, children)
-    : vnode(tag, {}, data)
+var vnode = function (name, data, children) {
+  return {
+    nodeName: name,
+    attributes: data,
+    children: Array.isArray(children) === true ? children : [children],
+    key: data.key
+  }
+}
 
-const html = new Proxy({}, {
-  get: (t, tag) => (data, children) => h(tag, data, children)
+var h = (name, data, children) => {
+  if (typeof data === 'object' && Array.isArray(data) === false) {
+    return vnode(name, data, children)
+  }
+
+  return vnode(name, {}, data)
+}
+
+var html = new Proxy({}, {
+  get: function (target, name) {
+    var fn = cache.get(name)
+
+    if (fn) {
+      return fn
+    }
+
+    fn = function (data, children) {
+      return h(name, data, children)
+    }
+
+    cache.set(fn)
+
+    return fn
+  }
 })
 
 export default html
